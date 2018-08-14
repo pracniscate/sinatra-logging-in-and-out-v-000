c67e7acb9c8c45aa6d737908b4c695c9e298dbbf
Lorena <lorena.giddens@outlook.com>
2018-08-14 13:06:47 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
 
-  def current_user
-
+  def current_user(hash)
+    #use the user_id from the session hash to find the user in the db & return that user
   end
 
-  def is_logged_in?
-
+  def is_logged_in?(hash)
+    #return true if the user_id is in the session hash, false if not
   end
 end
