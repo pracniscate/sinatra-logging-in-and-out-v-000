63179b0fe9b427196fa79cf7160c8c17905045dc
Lorena <lorena.giddens@outlook.com>
2018-08-14 12:39:01 +0000

    automatically backed up by learn

--- db/migrate/20180814123746_create_users.rb
+++ db/migrate/20180814123746_create_users.rb
@@ -1,4 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :breed
+      t.integer :balance
+    end
+  end
+
+  def down
+    drop_table :users
   end
 end
